ee26b05b311f602ae95bb2fdbfd77b6fa4eb3043
Sherlhy León López <sherlhy95@gmail.com>
2020-09-07 14:06:19 +0200

    The top-bar component was created and the navbar are displayed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MoviesComponent
+    MoviesComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
